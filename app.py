621c6992a0c5637fa9fc95fd60c200bf7556190b
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2020-02-23 20:50:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,204 +1,167 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author：Tyler%13 time:2019/12/5
-
+from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_bootstrap import Bootstrap
+from forms import *
+import os
+from werkzeug.utils import secure_filename
+from ImageOP import ImageProcess
 import numpy as np
-import cv2
-from PIL import Image,ImageEnhance,ImageDraw,ImageFont
-from skimage import io,color,transform,img_as_float,img_as_ubyte,img_as_uint
-from skimage.exposure import histogram,adjust_gamma,adjust_log,adjust_sigmoid,equalize_hist
-import matplotlib.pyplot as plt
 
-class ImageProcess():
-    def __init__(self):
-        self.img = []
-        self.im = []
-        self.img_cv = []
-        self.format = None
-        self.fromspace = None
-        self.temp = 0
-        self.orgFile = None
-        self.base_dir = "static/Image/"
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+bootstrap = Bootstrap(app)
+app.config["SECRET_KEY"] = "A-VERY-LONG-SECRET-KEY"
+filename = ""
+iop = ImageProcess()
 
-    def PhotoOpen(self,filename, change=False):
-        if change:
-            self.orgFile = filename
-            self.temp = 0
-        self.img = io.imread(filename)
-        self.im = Image.open(filename)
-        self.img_cv = cv2.imread(filename)
-        self.format = self.im.format
-        self.fromspace = self.im.mode
+@app.route("/", methods=["POST", "GET"])
+def index():
+    form = UpLoadFileForm()
+    global filename
+    print("in index")
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = os.path.join('static/Image', secure_filename(f.filename))
+        f.save(os.path.join(
+            basedir, filename
+        ))
+        print(filename)
+        try:
+            iop.PhotoOpen(filename, change=True)
+            flash("上传成功: " + filename, category="success")
+        # return redirect(url_for('index'))
+        except Exception as e:
+            flash("文件打开失败", category="danger")
+            flash(e, category="danger")
+    if filename == "":
+        flash("请打开图片", category="warning")
+    if filename:
+        iop.PhotoOpen(filename)
+    print("index filename", filename)
+    return render_template("index.html", upload_file_form=form, filename=filename + "?randomstr=" + str(np.random.rand()))
 
-    def ColorSpaceChange(self,toSpace):
-        self.temp = self.temp + 1
-        img = self.img
-        if self.fromspace == 'RGBA':
-            img = color.rgba2rgb(self.img)
-            self.fromspace = 'RGB'
-        converted_image = color.convert_colorspace(img,self.fromspace,toSpace) # having limits
-        io.imsave(self.base_dir+'temp'+str(self.temp)+'.'+self.format,converted_image)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
+@app.route("/basic", methods=["POST", "GET"])
+def basic():
+    global filename
+    if filename == "":
+        return redirect(url_for("index"))
+    rotate_form = RotateForm()
+    if rotate_form.validate_on_submit():
+        pass
+    if filename:
+        iop.PhotoOpen(filename)
+    return render_template("basic.html", filename=filename + "?randomstr=" + str(np.random.rand()), rotate_form=rotate_form)
 
-    def FftDct(self,op):
-        if op == 'fft':
-            print('in ImageOP.fft operation')
-            self.temp = self.temp + 1
-            img = color.rgb2gray(self.img)
-            img = np.float32(img)
-            fft_img = np.fft.fftshift(np.fft.fft2(img))
-            fft_img = np.log(np.abs(fft_img)+1)
-            io.imsave('static/Image/temp'+ str(self.temp)+'.'+self.format, fft_img)
-        elif op == 'dct':
-            print('in ImageOP.dct operation')
-            self.temp = self.temp + 1
-            img_cv = cv2.cvtColor(self.img_cv,cv2.COLOR_BGRA2GRAY)
-            img_cv = img_cv.astype(np.float32)
-            dct_img = cv2.dct(img_cv)
-            dct_img = np.log(np.abs(dct_img)+1e-5)
-            io.imsave('static/Image/temp'+ str(self.temp)+'.'+self.format, dct_img)
-        return 'static/Image/temp' + str(self.temp) + '.' + self.format
-
-    def Hist(self):
-        self.temp = self.temp + 1
-        if len(self.img.shape) == 2:
-            hist, hist_centers = histogram(self.img)
-            print(hist_centers)
-            plt.plot(hist_centers * 255, hist, lw=2)
+@app.route("/dip", methods=["POST", "GET"])
+def dip():
+    global filename
+    if filename == "":
+        return redirect(url_for("index"))
+    sharpen_form = SharpenForm()
+    if sharpen_form.validate_on_submit():
+        if sharpen_form.operation.data == 0:
+            filename = iop.BSCenhance(sharp_factor=sharpen_form.factor.data, choice="sharp")
+        elif sharpen_form.operation.data == 1:
+            filename = iop.BSCenhance(bright_factor=sharpen_form.factor.data, choice="bright")
         else:
-            for i in range(3):
-                hist, hist_centers = histogram(self.img[ :, :,i])
-                plt.plot(hist_centers,hist,lw=2)
-        plt.savefig('static/Image/temp'+str(self.temp)+'.'+self.format)
-        plt.clf()
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
-
-    def Hist_linear(self,a,b):
-        if len(self.img.shape) == 2:
-            img = self.img
-            for i in range(self.img.shape[0]):
-                for j in range(self.img.shape[1]):
-                    if (a * img[i,j] + b) > 255:
-                        img[i,j] = 255
-                    else:
-                        img[i,j] = a * img[i,j] + b
-                    img[i,j] = np.uint8(img[i,j])
-            io.imsave('static/Image/temp'+str(self.temp)+'.'+self.format, img)
+            filename = iop.BSCenhance(contrast_factor=sharpen_form.factor.data, choice="contrast")
+    filter_form = FilterForm()
+    if filter_form.validate_on_submit():
+        # filter_form.filtername.data
+        arr = ["guassian", "emboss", "edge", "sharp", "rect"]
+        filename = iop.filters(arr[filter_form.filtername.data])
+    linear_form = LinearForm()
+    if linear_form.validate_on_submit():
+        filename = iop.Hist_linear(a=linear_form.slope.data, b=linear_form.bias.data)
+    unlinear_form = UnLinearForm()
+    if unlinear_form.validate_on_submit():
+        if unlinear_form.function.data == 0:
+            filename = iop.Hist_gamma(1.2)
+        elif unlinear_form.function.data == 1:
+            filename = iop.Hist_sigmoid()
         else:
-            img = self.img
-            for i in range(self.img.shape[2]):
-                for j in range(self.img.shape[0]):
-                    for k in range(self.img.shape[1]):
-                        if (a * img[j, k,i] + b) > 255:
-                            img[j,k,i] = 255
-                        else:
-                            img[j,k,i] = a * img[j,k,i] + b
-                        img[j,k,i] = np.uint8(img[j,k,i])
-            io.imsave('static/Image/temp'+str(self.temp)+'.'+self.format, img)
-        return 'static/Image/temp' + str(self.temp) + '.' + self.format
+            filname = iop.Hist_log()
+    color_space_reverse_form = ColorSpaceReverseForm()
+    if color_space_reverse_form.validate_on_submit():
+        arr = ['RGB', 'HSV', 'RGB CIE', 'XYZ', 'YUV', 'YIQ', 'YPbPr', 'YCbCr']
+        filename = iop.ColorSpaceChange(arr[color_space_reverse_form.toSpace.data])
+    if filename:
+        iop.PhotoOpen(filename)
+    return render_template("dip.html", filename=filename + "?randomstr=" + str(np.random.rand()), 
+                    sharpen_form=sharpen_form, filter_form=filter_form, 
+                    linear_form=linear_form, unlinear_form=unlinear_form,
+                    color_space_reverse_form=color_space_reverse_form)
 
-    def Hist_gamma(self,gamma,gain = 1):
-        self.temp = self.temp + 1
-        img = img_as_float(self.img)
-        gamma_corrected = adjust_gamma(img,gamma,gain)
-        io.imsave('static/Image/temp'+ str(self.temp)+'.'+self.format, gamma_corrected)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
-    def Hist_sigmoid(self):
-        self.temp = self.temp + 1
-        img = img_as_float(self.img)
-        sigmoid_corrected = adjust_sigmoid(img)
-        io.imsave('static/Image/temp'+ str(self.temp)+'.'+ self.format, sigmoid_corrected)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
-    def Hist_log(self):
-        self.temp = self.temp + 1
-        img = img_as_float(self.img)
-        log_corrected = adjust_log(img)
-        io.imsave('static/Image/temp'+ str(self.temp)+'.'+self.format, log_corrected)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
-    def Hist_equalize(self):
-        self.temp = self.temp + 1
-        img = img_as_ubyte(self.img)
-        img = equalize_hist(img)
-        io.imsave('static/Image/temp'+ str(self.temp)+'.'+self.format, img)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
+@app.route("/equalization", methods=["POST", "GET"])
+def equalization():
+    global filename
+    filname = iop.Hist_equalize()
+    return redirect(url_for("dip"))
 
-    def RotateOP(self,anger = 90):
-        self.temp = self.temp + 1
-        img = transform.rotate(self.img,anger)
-        io.imsave('static/Image/temp'+ str(self.temp)+'.'+self.format, img)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
-    def ResizeOP(self,size):
-        self.temp = self.temp + 1
-        img = transform.resize(self.img,size)
-        io.imsave('static/Image/temp'+str(self.temp)+'.'+self.format,img)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
+@app.route("/smooth", methods=["POST", "GET"])
+def smooth():
+    global filename
+    filename = iop.filters("rect")
+    return redirect(url_for("dip"))
 
-    def WordAdd(self,area,contend,color,size):
-        self.temp = self.temp + 1
-        setFont = ImageFont.truetype('simfang.ttf',size)
-        im = self.im
-        draw = ImageDraw.Draw(im)
-        draw.text(area,contend,color,font =setFont)
-        im.save('static/Image/temp'+str(self.temp)+'.'+self.format)
-        return 'static/Image/temp'+str(self.temp)+'.'+self.format
+@app.route("/histogram", methods=['POST', 'GET'])
+def histogram():
+    print("in histogram")
+    hist = iop.Hist()
+    return hist + "?randomstr=" + str(np.random.rand())
 
-    def BSCenhance(self,bright_factor = 1.2, sharp_factor = 1.2, contrast_factor = 1.2, choice = 'bright'):
-        self.temp = self.temp+1
-        if choice == 'sharp':
-            Simg = ImageEnhance.Sharpness(self.im)
-            Simg = Simg.enhance(sharp_factor)
-            Simg.save('static/Image/temp' + str(self.temp) + '.' + self.format)
-        elif choice == 'contrast':
-            Cimg = ImageEnhance.Contrast(self.im)
-            Cimg = Cimg.enhance(contrast_factor)
-            Cimg.save('static/Image/temp' + str(self.temp) + '.' + self.format)
-        elif choice == 'bright':
-            Bimg = ImageEnhance.Brightness(self.im)
-            Bimg = Bimg.enhance(bright_factor)
-            Bimg.save('static/Image/temp' + str(self.temp) + '.' + self.format)
+@app.route("/FFTDCT", methods=["POST", "GET"])
+def FFTDCT():
+    print("in fft dct", request.form.get("operation"))
+    ret = iop.FftDct(request.form.get("operation"))
+    print(ret)
+    return ret + "?randomstr=" + str(np.random.rand())
 
-        return 'static/Image/temp'+ str(self.temp)+'.'+self.format
+@app.route("/org", methods=["GET"])
+def org():
+    global filename
+    print(filename)
+    filename = iop.orgFile
+    print(filename)
+    iop.PhotoOpen(filename, change=True)
+    return redirect(url_for("index"))
 
-    def filters(self,filtername):
-        kernel1 = np.array([[.11,.11,.11],[.11,.11,.11],[.11,.11,.11]])
-        kernel_gaussian = np.array([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4],[1,4,7,4,1]]) / 273
-        kernel_emboss = np.array([[-2,-2,-2,-2,0],[-2,-2,-2,0,2],[-2,-2,0,2,2],[-2,0,2,2,2],[0,2,2,2,2]])
-        kernel_edge = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
-        kernel_sharp = np.array([[0,-2,0],[-2,9,-2],[0,-2,0]])
+@app.route("/undo", methods=["GET", "POST"])
+def undo():
+    global filename
+    if iop.temp == 1:
+        filename = iop.orgFile
+        iop.PhotoOpen(iop.orgFile)
+    else:
+        iop.temp -= 1
+        filename = iop.base_dir + 'temp'+str(iop.temp)+'.'+iop.format
+        print("in undo")
+        print(filename)
+        iop.PhotoOpen(filename)
+    return redirect(url_for("index"))
 
-        if filtername == 'gaussian':
-            self.temp = self.temp + 1
-            gaussian = cv2.filter2D(self.img_cv, -1, kernel_gaussian)
-            cv2.imwrite('static/Image/temp'+ str(self.temp)+'.'+self.format, gaussian)
-        elif filtername == 'emboss':
-            self.temp = self.temp + 1
-            emboss = cv2.filter2D(self.img_cv, -1, kernel_emboss)
-            emboss = cv2.cvtColor(emboss, cv2.COLOR_BGR2GRAY)
-            cv2.imwrite('static/Image/temp'+ str(self.temp)+'.'+self.format, emboss)
-        elif filtername == 'edge':
-            self.temp = self.temp + 1
-            edge = cv2.filter2D(self.img_cv, -1, kernel_edge)
-            cv2.imwrite('static/Image/temp'+ str(self.temp)+'.'+self.format, edge)
-        elif filtername == 'sharp':
-            self.temp = self.temp + 1
-            sharp = cv2.filter2D(self.img_cv, -1, kernel_sharp)
-            cv2.imwrite('static/Image/temp'+ str(self.temp)+'.'+ self.format, sharp)
-        elif filtername == 'rect':
-            self.temp = self.temp + 1
-            rect = cv2.filter2D(self.img_cv,-1,kernel1)
-            cv2.imwrite('static/Image/temp'+ str(self.temp)+'.'+self.format,rect)
-        else:
-            pass
-        return 'static/Image/temp'+ str(self.temp)+'.'+self.format
+@app.route("/redo", methods=["GET", "POST"])
+def redo():
+    try:
+        global filename
+        iop.temp += 1
+        iop.PhotoOpen(iop.base_dir + 'temp'+str(iop.temp)+'.'+iop.format)
+        filename = iop.base_dir + 'temp'+str(iop.temp)+'.'+iop.format
+    except:
+        iop.temp -= 1
+        flash("已经是最新操作！", category="warning")
+    return redirect(url_for("index"))
 
-# if __name__ == '__main__':
-#     c = ImageProcess()
-#     c.PhotoOpen('./image/1.png')
-#     c.ColorSpaceChange('hsv')
-#     c.FftDct('dct')
-#     c.Hist()
-#     c.BSCenhance(1,10,1,'sharp')
-#     #c.Hist_linear(3,6)
-#     c.filters('emboss')
-#     c.Hist_equalize()
-#     c.WordAdd((40,40),'HAPPY',(255,255,255),50)
+@app.route("/rotate1")
+def rotate1():
+    global filename
+    filename = iop.RotateOP(90)
+    return redirect(url_for("basic"))
+
+@app.route("/rotate2")
+def rotate2():
+    global filename
+    filename = iop.RotateOP(-90)
+    return redirect(url_for("basic"))
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
